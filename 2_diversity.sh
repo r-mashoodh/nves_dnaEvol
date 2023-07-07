48e48b818b436b23ece3162c531e3c35f4740ee8
Rahia Mashoodh <17982017+r-mashoodh@users.noreply.github.com>
2023-07-07 10:21:22 +0100

    Update 2_diversity.sh

--- 2_diversity.sh
+++ 2_diversity.sh
@@ -7,7 +7,7 @@ samtools mpileup -B ../map/F2.qf.rmd.sort.bam > F2.pileup
 samtools mpileup -B ../map/N2.qf.rmd.sort.bam > N2.pileup
 
 
- filtering indels
+# filtering indels
 perl ../software/popoolation_1.2.2/basic-pipeline/identify-genomic-indel-regions.pl --indel-window 5 --min-count 2 --input F1.pileup --output F1.indels.gtf
 perl ../software/popoolation_1.2.2/basic-pipeline/identify-genomic-indel-regions.pl --indel-window 5 --min-count 2 --input N1.pileup --output N1.indels.gtf
 perl ../software/popoolation_1.2.2/basic-pipeline/identify-genomic-indel-regions.pl --indel-window 5 --min-count 2 --input F2.pileup --output F2.indels.gtf
@@ -18,6 +18,11 @@ perl ../software/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --inpu
 perl ../software/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --input F2.pileup --gtf F2.indels.gtf --output F2.idf.pileup
 perl ../software/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --input N2.pileup --gtf N2.indels.gtf --output N2.idf.pileup
 
+# filtering TEs
+perl ../software/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --input F1.idf.pileup --gtf n_vespilloides.filteredRepeats.gff --output F1.idf.tef.pileup
+perl ../software/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --input F2.idf.pileup --gtf n_vespilloides.filteredRepeats.gff --output F2.idf.tef.pileup
+perl ../software/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --input N1.idf.pileup --gtf n_vespilloides.filteredRepeats.gff --output N1.idf.tef.pileup
+perl ../software/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --input N2.idf.pileup --gtf n_vespilloides.filteredRepeats.gff --output N2.idf.tef.pileup
 
 ##CALCULATING TAJIMA’S / PI DIVERSITY
 ## –min-coverage –max-coverage: for subsampled files not important; should contain target coverage, i.e.: 10
@@ -30,14 +35,20 @@ perl ../software/popoolation_1.2.2/basic-pipeline/filter-pileup-by-gtf.pl --inpu
 ## –snp-output a file containing for every window the SNPs that have been used for computing the measure (e.g.)
 ## –window-size –step-size control behaviour of sliding window; if step size is smaller than window size than the windows will be overlapping.
 
-perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure pi --input N1.idf.pileup --min-count 2 --min-coverage 40 --max-coverage 500 --min-covered-fraction 0.25 --pool-size 104 --window-size 1000 --step-size 1000 --output N1.1000.pi --snp-output N1.1000.snps
-perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure pi --input F2.idf.pileup --min-count 2 --min-coverage 40 --max-coverage 500 --min-covered-fraction 0.25 --pool-size 104 --window-size 1000 --step-size 1000 --output F2.1000.pi --snp-output F2.1000.snps
-perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure pi --input N2.idf.pileup --min-count 2 --min-coverage 40 --max-coverage 500 --min-covered-fraction 0.25 --pool-size 118 --window-size 1000 --step-size 1000 --output N2.1000.pi --snp-output N2.1000.snps
-perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure pi --input F1.idf.pileup --min-count 2 --min-coverage 40 --max-coverage 500 --min-covered-fraction 0.25 --pool-size 82 --window-size 1000 --step-size 1000 --output F1.1000.pi --snp-output F1.1000.snps
+# pi
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure pi --input N1.idf.tef.pileup --min-count 4 --min-coverage 40 --max-coverage 600 --min-covered-fraction 0.25 --pool-size 104 --window-size 1000 --step-size 1000 --output tef_out/N1.1000.tef.pi --snp-output tef_out/N1.1000.tef.snps
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure pi --input F2.idf.tef.pileup --min-count 4 --min-coverage 40 --max-coverage 600 --min-covered-fraction 0.25 --pool-size 104 --window-size 1000 --step-size 1000 --output tef_out/F2.1000.tef.pi --snp-output tef_out/F2.1000.tef.snps
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure pi --input N2.idf.tef.pileup --min-count 4 --min-coverage 40 --max-coverage 600 --min-covered-fraction 0.25 --pool-size 118 --window-size 1000 --step-size 1000 --output tef_out/N2.1000.tef.pi --snp-output tef_out/N2.1000.tef.snps
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure pi --input F1.idf.tef.pileup --min-count 4 --min-coverage 40 --max-coverage 600 --min-covered-fraction 0.25 --pool-size 82 --window-size 1000 --step-size 1000 --output tef_out/F1.1000.tef.pi --snp-output tef_out/F1.1000.tef.snps
 
-perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure theta --input N1.idf.pileup --min-count 2 --min-coverage 40 --max-coverage 500 --min-covered-fraction 0.25 --pool-size 104 --window-size 1000 --step-size 1000 --output N1.1000.theta --snp-output N1.1000.snps
-perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure theta --input F2.idf.pileup --min-count 2 --min-coverage 40 --max-coverage 500 --min-covered-fraction 0.25 --pool-size 104 --window-size 1000 --step-size 1000 --output F2.1000.theta --snp-output F2.1000.snps
-perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure theta --input N2.idf.pileup --min-count 2 --min-coverage 40 --max-coverage 500 --min-covered-fraction 0.25 --pool-size 118 --window-size 1000 --step-size 1000 --output N2.1000.theta --snp-output N2.1000.snps
-perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure theta --input F1.idf.pileup --min-count 2 --min-coverage 40 --max-coverage 500 --min-covered-fraction 0.25 --pool-size 82 --window-size 1000 --step-size 1000 --output F1.1000.theta --snp-output F1.1000.snps
+# theta
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure theta --input N1.idf.tef.pileup --min-count 4 --min-coverage 40 --max-coverage 600 --min-covered-fraction 0.25 --pool-size 104 --window-size 1000 --step-size 1000 --output tef_out/N1.1000.tef.theta
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure theta --input F2.idf.tef.pileup --min-count 4 --min-coverage 40 --max-coverage 600 --min-covered-fraction 0.25 --pool-size 104 --window-size 1000 --step-size 1000 --output tef_out/F2.1000.tef.theta
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure theta --input N2.idf.tef.pileup --min-count 4 --min-coverage 40 --max-coverage 600 --min-covered-fraction 0.25 --pool-size 118 --window-size 1000 --step-size 1000 --output tef_out/N2.1000.tef.theta
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure theta --input F1.idf.tef.pileup --min-count 4 --min-coverage 40 --max-coverage 600 --min-covered-fraction 0.25 --pool-size 82 --window-size 1000 --step-size 1000 --output tef_out/F1.1000.tef.theta
 
-
+# tajima's d (500bp sliding to compare to Fst windows)
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure D --input N1.idf.tef.pileup --min-count 1 --min-coverage 20 --max-coverage 600 --min-covered-fraction 0.3 --pool-size 104 --window-size 500 --step-size 250 --output tef_out/N1.500.ovlp.tef.td --dissable-corrections
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure D --input F2.idf.tef.pileup --min-count 1 --min-coverage 20 --max-coverage 600 --min-covered-fraction 0.3 --pool-size 104 --window-size 500 --step-size 250 --output tef_out/F2.500.ovlp.tef.td --dissable-corrections
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure D --input N2.idf.tef.pileup --min-count 1 --min-coverage 20 --max-coverage 600 --min-covered-fraction 0.3 --pool-size 118 --window-size 500 --step-size 250 --output tef_out/N2.500.ovlp.tef.td --dissable-corrections
+perl ../software/popoolation_1.2.2/Variance-sliding.pl --fastq-type sanger --measure D --input F1.idf.tef.pileup --min-count 1 --min-coverage 20 --max-coverage 600 --min-covered-fraction 0.3 --pool-size 82 --window-size 500 --step-size 250 --output tef_out/F1.500.ovlp.tef.td --dissable-corrections
